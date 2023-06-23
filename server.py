0c719e24fb502d4d3f70344b601862fc11e91380
YuryOAraujo <yuryaraujo02@gmail.com>
2023-06-23 13:27:36 -0300

    Inclusão de thread no servidor.

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import socket
 import time
-import sys
+import threading
 
 # Configurações do servidor
 HOST = 'localhost'
@@ -9,31 +9,38 @@ PORT = 5000
 # Cria o socket do servidor
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((HOST, PORT))
-server_socket.listen(1)
+server_socket.listen(0)
 
-print('Aguardando conexão do cliente...')
-
-# Aceita a conexão do cliente
-client_socket, addr = server_socket.accept()
-print('Conexão estabelecida com', addr)
+print('Aguardando conexões dos clientes...')
 
 # Função para enviar o tempo atual ao cliente
-def send_current_time():
-    try:
-        current_time = time.ctime(time.time())
-        client_socket.send(current_time.encode())
-    except ConnectionAbortedError:
-        print("Conexão com o cliente encerrada, finalizando o servidor ...")
-        sys.exit()
+def send_current_time(client_socket):
+    while True:
+        try:
+            current_time = time.ctime(time.time())
+            client_socket.send(current_time.encode())
+        except ConnectionAbortedError:
+            break
+        except ConnectionResetError:
+            print("Conexão com o cliente redefinida")
+            break
+        time.sleep(1)
 
-# Envia o tempo atual a cada segundo
+# Função para lidar com cada cliente em uma nova thread
+def handle_client(client_socket, client_address):
+    print('Conexão estabelecida com', client_address)
+    send_current_time(client_socket)
+    client_socket.close()
+    print('Conexão encerrada com', client_address)
+
+# Aceita conexões dos clientes e cria uma nova thread para cada cliente
 while True:
     try:
-        send_current_time()
-    except ConnectionAbortedError:
+        client_socket, client_address = server_socket.accept()
+        client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+        client_thread.start()
+    except KeyboardInterrupt:
         break
-    time.sleep(1)
 
-# Fecha a conexão
-client_socket.close()
+# Encerra o socket do servidor
 server_socket.close()
